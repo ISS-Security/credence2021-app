d0c27a2917269f6a72a6e25efacae3f6138451d6
Soumya Ray <soumya.ray@gmail.com>
2021-05-20 15:55:19 +0800

    Secure sessions and simple registration

--- config/environments.rb
+++ config/environments.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 
+require 'delegate'
 require 'roda'
 require 'figaro'
 require 'logger'
+require 'rack/ssl-enforcer'
+require 'rack/session/redis'
+require_relative '../require_app'
+
+require_app('lib')
 
 module Credence
   # Configuration for the API
@@ -20,6 +26,33 @@ module Credence
     # Logger setup
     LOGGER = Logger.new($stderr)
     def self.logger() = LOGGER
+
+    ONE_MONTH = 30 * 24 * 60 * 60
+
+    configure do
+      SecureSession.setup(ENV['REDIS_URL']) # REDIS_URL used again below
+      SecureMessage.setup(ENV.delete('MSG_KEY'))
+    end
+
+    configure :production do
+      use Rack::SslEnforcer, hsts: true
+
+      use Rack::Session::Redis,
+          expire_after: ONE_MONTH,
+          redis_server: ENV.delete('REDIS_URL')
+    end
+
+    configure :development, :test do
+      # use Rack::Session::Cookie,
+      #     expire_after: ONE_MONTH, secret: config.SESSION_SECRET
+
+      use Rack::Session::Pool,
+          expire_after: ONE_MONTH
+
+      # use Rack::Session::Redis,
+      #     expire_after: ONE_MONTH,
+      #     redis_server: ENV.delete('REDIS_URL')
+    end
 
     configure :development, :test do
       require 'pry'
